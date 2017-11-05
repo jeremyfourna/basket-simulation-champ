4cd7b169c0ea545b350f3489375f68b00b555db7
jeremyfourna <jeremyfourna@gmail.com>
2017-11-05 15:03:29 +0100

    add style for championship

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const R = require('ramda');
 const { generateId } = require('basket-simulation-utils');
 const { generateClub } = require('basket-simulation-club');
 
-function generateChampionship(level, nbClubs = 10) {
+function generateChampionship(style, level, nbClubs = 10) {
   const clubsForChamp = R.map(generateClub, R.range(0, nbClubs));
 
   return [{
+    style,
     level,
     id: generateId(),
     clubs: R.map(R.prop('id'), clubsForChamp)
