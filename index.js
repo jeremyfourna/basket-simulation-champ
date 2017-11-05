a0169c143e134e043f93512cdb778d2675d33bff
jeremyfourna <jeremyfourna@gmail.com>
2017-11-05 14:50:24 +0100

    Return also teams for the champ

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const { generateClub } = require('basket-simulation-club');
 function generateChampionship(level, nbClubs = 10) {
   const clubsForChamp = R.map(generateClub, R.range(0, nbClubs));
 
-  return {
+  return [{
     level,
     id: generateId(),
     clubs: R.map(R.prop('id'), clubsForChamp)
-  };
+  }, clubsForChamp];
 }
 
 exports.generateChampionship = generateChampionship;
